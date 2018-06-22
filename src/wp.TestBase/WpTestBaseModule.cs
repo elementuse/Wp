81f3ba8549207f20aa1b1e771bbaf75e019cd557
王导 <elementuse@live.cn>
2018-06-22 13:32:00 +0800

    init

---
+++ src/wp.TestBase/WpTestBaseModule.cs
@@ -0,0 +1,18 @@
+﻿using wp.Modules;
+
+namespace wp
+{
+    [DependsOn(typeof(WpKernelModule))]
+    public class WpTestBaseModule : WpModule
+    {
+        public override void PreInitialize()
+        {
+            base.PreInitialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
---
+++ src/wp.Web.Common/Web/WpWebCommonModule.cs
@@ -0,0 +1,18 @@
+﻿using wp.Modules;
+
+namespace wp.Web
+{
+    [DependsOn(typeof(WpKernelModule))]
+    public class WpWebCommonModule : WpModule
+    {
+        public override void PreInitialize()
+        {
+            base.PreInitialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
